c5082a2ec219d6017704680bb11707f471dcfdbe
mjfalahee <mjfalahee@gmail.com>
2019-09-11 14:25:49 -0700

    Working on eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,25 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  ways_to_eat = 0
+  #Base case == 1
+  if n == 1:
+    return 1
+
+  elif n < 1:
+    return 0
+
+  elif n in cache:
+    print(cache[n])
+    return cache[n]
+
+  #I think the recursive soluton is eating_cookies(n) == (eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
+  else:
+    ways_to_eat = (eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache))
+
+  return ways_to_eat
+    
+
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
