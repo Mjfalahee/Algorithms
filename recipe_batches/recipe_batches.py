f14d02d8fc04cc14d489e5f9ed62605515f444b8
mjfalahee <mjfalahee@gmail.com>
2019-09-11 13:40:39 -0700

    Working on recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,15 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+
+  #takes recipe first, then ingredients
+  #I want to separate values from keys, so I can quickly evaluate if all of the recipe ingredients exist
+  rec_keys = list(recipe.keys())
+  ingredient_keys = list(ingredients.keys())
+
+  #if the recipes keys are longer than the ingredient keys, it's an auto fail
+  if len(rec_keys) > len(ingredient_keys):
+    return 0
 
 
 if __name__ == '__main__':
