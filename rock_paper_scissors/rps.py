98be706937b3a736eaf24b8a3da70aad7fc07df8
mjfalahee <mjfalahee@gmail.com>
2019-09-11 16:20:35 -0700

    started work on rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,19 @@
 
 import sys
 
+#define a list with all of the plays.
+plays = ['rock', 'paper', 'scissors']
 def rock_paper_scissors(n):
-  pass 
+
+  # recursion
+  # List of results. All of length n. All with different values. 
+  # cache?
+  # Concatenate two lists with the + operator
+
+  inner_lists = []
+  #all of the inner_lists are of length n
+  result_list = [inner_lists]
+  return result_list
 
 
 if __name__ == "__main__":
