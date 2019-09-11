30c75c662deb83e3f3228a136e77e0c8eb347d64
mjfalahee <mjfalahee@gmail.com>
2019-09-11 14:35:45 -0700

    Small test cases for eating_cookies.py pass

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -10,18 +10,27 @@ def eating_cookies(n, cache=None):
   #Base case == 1
   if n == 1:
     return 1
-
-  elif n < 1:
+  
+  #I don't understand why the test case has zero cookies with a way to eat them, but I'm accounting for that here.
+  elif n == 0:
+    return 1
+  
+  #if there aren't cookies, you can't eat them.
+  elif n < 0:
     return 0
 
-  elif n in cache:
-    print(cache[n])
-    return cache[n]
+  #check if it's in the cache
+  # elif n in cache:
+  #   return cache[n]
 
   #I think the recursive soluton is eating_cookies(n) == (eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
   else:
-    ways_to_eat = (eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache))
+   result  = (eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
+  #  print('Result', result)
 
+
+  ways_to_eat = result
+  # cache[n] = ways_to_eat
   return ways_to_eat
     
 
