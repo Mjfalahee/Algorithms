d81cedb206885c9cd090e231b1a4c59836bc01bb
mjfalahee <mjfalahee@gmail.com>
2019-09-11 15:14:05 -0700

    Changed my code to address the test cases. Fixed cache.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,6 +5,7 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
 def eating_cookies(n, cache=None):
   ways_to_eat = 0
   #Base case == 1
@@ -20,18 +21,17 @@ def eating_cookies(n, cache=None):
     return 0
 
   #check if it's in the cache
-  # elif n in cache:
-  #   return cache[n]
+  elif cache and (cache[n] > 0):
+    return cache[n]
 
   #I think the recursive soluton is eating_cookies(n) == (eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
   else:
-   result  = (eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
-  #  print('Result', result)
-
-
-  ways_to_eat = result
-  # cache[n] = ways_to_eat
-  return ways_to_eat
+    if not cache:
+      cache = {i: 0 for i in range(0, n+1)}
+    result = (eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache))
+    cache[n] = result
+  
+  return cache[n]
     
 
 
