52ee686cb38e5b2ea32396cd5a98f199499826ed
mjfalahee <mjfalahee@gmail.com>
2019-09-12 13:52:34 -0700

    RPS is working. Tests pass. Ended up using a helper array because it made more sense for me

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,19 +2,28 @@
 
 import sys
 
-#define a list with all of the plays.
-plays = ['rock', 'paper', 'scissors']
+
 def rock_paper_scissors(n):
+  #Define all possible plays in a list
+  plays = ['rock', 'paper', 'scissors']
 
-  # recursion
-  # List of results. All of length n. All with different values. 
-  # cache?
-  # Concatenate two lists with the + operator
+  #Define a list to hold all of the possible plays
+  result = []
 
-  inner_lists = []
-  #all of the inner_lists are of length n
-  result_list = [inner_lists]
-  return result_list
+  def rpshelper(n, rps):
+    #base case or ceiling, if (len(rps) == 0) ==> result.append([]) ==> result = [[]]
+    #if ceiling (rps has been hit length n), append it to the result array
+    if len(rps) == n:
+      return result.append(rps)
+
+    #iterate through possible plays, push rps + [i] together to make [rps, i]
+    #and send back through recursion until rps is of length n
+    for i in plays:
+      rpshelper(n, rps + [i])
+  
+  #invoke helper to start recursion with a blank array
+  rpshelper(n, [])
+  return result
 
 
 if __name__ == "__main__":
@@ -23,3 +32,17 @@ if __name__ == "__main__":
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
+
+  # 0 - [[]]
+  # 1 - [['rock'], ['paper'], ['scissors']]
+  # 2 - [['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'], ['paper', 'rock'], ['paper', 'paper'], ['paper', 'scissors'], ['scissors', 'rock'], ['scissors', 'paper'], ['scissors', 'scissors']]
+
+
+  # 0 - [[]] 1
+  # 1 - [[1], [2], [3]] 3
+  # 2 - [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]] 9
+  # 3 - [[]] 27
+  # 4 - [[]] 81
+  # 5 - [[]] 243
+
+  # relationship is (n-1)*3
